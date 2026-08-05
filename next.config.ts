841a73ac397afce06c183d2016b5e1e6f99affa7
okta <fachrudinokta@gmail.com>
2026-08-05 14:21:02 +0700

    chore: update Content Security Policy to include FontAwesome and Google Fonts sources

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,15 @@ import type { NextConfig } from "next";
 
 const cspHeader = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com;
-  style-src 'self' 'unsafe-inline';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com https://kit.fontawesome.com;
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
   img-src 'self' blob: data: https://lh3.googleusercontent.com;
-  font-src 'self';
+  font-src 'self' data: https://fonts.gstatic.com https://ka-f.fontawesome.com;
   object-src 'none';
   base-uri 'self';
   form-action 'self';
   frame-ancestors 'none';
-  connect-src 'self' https://*.googleapis.com https://*.firebaseio.com wss://*.firebaseio.com https://*.firebaseapp.com https://generativelanguage.googleapis.com;
+  connect-src 'self' https://*.googleapis.com https://*.firebaseio.com wss://*.firebaseio.com https://*.firebaseapp.com https://generativelanguage.googleapis.com https://ka-f.fontawesome.com;
 `;
 
 const nextConfig: NextConfig = {
